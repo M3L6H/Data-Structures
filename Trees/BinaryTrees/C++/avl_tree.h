8bd530edfbeccfbd4fcebe6dcb21dc91a8e32d37
M3L6H <sisoac@hotmail.com>
2020-09-29 18:45:12 -0700

    Flesh out AVL Tree definition

--- Trees/BinaryTrees/C++/avl_tree.h
+++ Trees/BinaryTrees/C++/avl_tree.h
@@ -12,6 +12,22 @@ template <class T> class AVLTree {
     explicit AVLTree();
     explicit AVLTree(Comp comp);
 
+    // Check whether the tree contains the given value
+    bool Contains(const T& value) const;
+
+    // Insert an element into the tree
+    // Returns true if the insertion was successful (i.e. the tree did not
+    // already contain value)
+    bool Insert(const T& value);
+
+    // Delete an element from the tree
+    // Returns true if the deletion was successful (i.e. the tree contained the
+    // given value)
+    bool Delete(const T& value);
+
+    // Returns the number of elements currently in the tree
+    int size() const;
+
   private:
     // Internal node struct used to maintain tree structure
     struct Node {
@@ -20,6 +36,25 @@ template <class T> class AVLTree {
       Node* right;
       int height;
     };
+
+    // Creates a new blank node with the given value and increments size
+    // accordingly
+    Node* CreateNode(const T& value);
+
+    // Finds the node with the closest value to value
+    // Will always return a non-null pointer except when the tree is empty
+    Node* FindClosest(const T& value) const;
+
+    // Returns the in-order successor of the given node
+    Node* InOrderSuccessor(Node* node) const;
+
+    // Rotates the given nodes to the right
+    void RotateRight(Node* parent, Node* child);
+
+    // Rotates the given nodes to the left
+    void RotateLeft(Node* parent, Node* child);
+
+    int size_;
 
     Node* root_;
     Comp comp_;
