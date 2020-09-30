0a3965f02d0b17329727d64aaad51dbe99e67555
M3L6H <sisoac@hotmail.com>
2020-09-29 20:54:31 -0700

    Add implementation for constructor

--- Trees/BinaryTrees/C++/avl_tree.cc
+++ Trees/BinaryTrees/C++/avl_tree.cc
@@ -1,1 +1,14 @@
 #include "avl_tree.h";
+
+template <class T>
+AVLTree<T>::AVLTree() : root_(nullptr), size(0) {
+  comp_ = [](T a, T b) {
+    if (a < b) {
+      return -1;
+    } else if (a == b) {
+      return 0;
+    }
+    
+    return 1;
+  };
+}
