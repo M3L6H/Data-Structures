0c81c7d00f3beb4ce4d4ef39a3e13b09b6b61112
M3L6H <sisoac@hotmail.com>
2020-09-30 18:38:25 -0700

    Add testing script

---
+++ Trees/BinaryTrees/C++/val_bst_tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+valgrind --leak-check=full \
+         --show-leak-kinds=all \
+         --track-origins=yes \
+         --verbose \
+         --log-file=valgrind-out.txt \
+         ./cmake-build-vs/test/BinaryTreesTests 
