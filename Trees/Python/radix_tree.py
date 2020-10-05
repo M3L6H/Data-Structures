0e37862e8e630511a32509b40a0766fee2d384fb
M3L6H <sisoac@hotmail.com>
2020-10-05 16:52:00 -0700

    Add insert method

--- Trees/Python/radix_tree.py
+++ Trees/Python/radix_tree.py
@@ -1,6 +1,70 @@
 class RadixTree:
   def __initialize__(self):
-    self.root = Node(False)
+    self.root = self.Node(False)
+
+  def insert(self, value):
+    # We don't permit inserting blank values
+    if len(value) == 0:
+      return False
+
+    # Start at the root
+    curr_node = self.root
+    curr_index = 0
+
+    # As long as we have not reached the end of our value, continue
+    while curr_index < len(value):
+      # We reached a leaf node, meaning the value is already in our tree      
+      if curr_node.leaf:
+        return False
+
+      # Track whether our current index changed
+      # i.e. whether we found a matching prefix
+      change = 0
+      
+      # Look through all the edges in our current node
+      for edge in curr_node.edges:
+        # Calculate the prefix
+        pref = self.is_prefix(edge.value, value[curr_index:])
+
+        # Prefix greater than 0 means there was a match up to pref
+        if pref > 0:
+          # If pref is as long as edge.value, it means that edge.value was a
+          # prefect prefix (i.e. it fit completely)
+          if pref == len(edge.value):          
+            # So we update our flag and current node and break out of this
+            # inner loop, since there can be at most one matching prefix
+            change = pref
+            curr_node = edge.node
+            break
+          # Otherwise there was a mismatch at some point, but at least one
+          # character matched
+          else:
+            # So we split the edge at the trouble point, inserting the new
+            # value in the process
+            edge.split(pref, value[(curr_index + pref - 1):])
+            return True
+        # If pref is negative, it means there was an over-match. In other words
+        # the value we are trying to insert is itself a prefix of edge.value
+        # So we split the offending edge and insert a blank edge for our new
+        # value
+        elif pref < 0:
+          edge.split(len(edge.value) + pref)
+          return True
+
+      # Adjust the current index by our change
+      curr_index += change
+
+      # If we did not change at all, it means that no matching prefix was found
+      # in the last iteration of this loop, so we should break and insert a new
+      # edge
+      if change == 0:
+        break
+
+    # Insert a new edge with any remaining characterrs in our value
+    new_edge = self.Edge(value[curr_index:], self.Node())
+    curr_node.add_edge(new_edge)
+    return True
+
 
   def is_prefix(self, a, b):
     idx = 0
@@ -22,7 +86,28 @@ class RadixTree:
       self.leaf = leaf
       self.edges = []
 
+    def add_edge(self, edge):
+      self.edges.append(edge)
+
   class Edge:
-    def __initialize__(self, prefix, node):
-      self.prefix = prefix
+    def __initialize__(self, value, node):
+      self.value = value
       self.node = node
+
+    def split(self, index, new_prefix=""):
+      if index >= len(self.value) or index == 0:
+        raise IndexError(f"Index { index } is out of bounds of 1-{ len(self.value) }")
+
+      # Create new edges
+      next_edge = Edge(self.value[index:], self.node)
+      leaf_node = Node()
+      branch_edge = Edge(new_prefix, leaf_node)
+
+      # Create branch node
+      branch_node = Node(False)
+      branch_node.add_edge(next_edge)
+      branch_node.add_edge(branch_edge)
+
+      # Update current edge
+      self.value = self.value[0:index]
+      self.node = branch_node
