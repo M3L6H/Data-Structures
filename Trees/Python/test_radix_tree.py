d55f3e0026084953149a91b6ea7aae53b378f702
M3L6H <sisoac@hotmail.com>
2020-10-04 21:55:17 -0700

    Create test for is_prefix method

---
+++ Trees/Python/test_radix_tree.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest import TestCase
+
+from radix_tree import RadixTree
+
+class TestRadixTree(TestCase):
+  def setUp(self):
+    self.radix_tree = RadixTree()
+  
+  def test_is_prefix(self):
+    self.assertEqual(0, self.radix_tree.is_prefix("apple", "pear"))
+    self.assertEqual(4, self.radix_tree.is_prefix("apple", "appliance"))
+    self.assertEqual(5, self.radix_tree.is_prefix("cards", "cards"))
+    self.assertEqual(-1, self.radix_tree.is_prefix("cards", "card"))
+    self.assertEqual(-5, self.radix_tree.is_prefix("cardistry", "card"))
+
+
+if __name__ == "__main__":
+  unittest.main()
