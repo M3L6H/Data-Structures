2428dad213cfef2242c05a6e9eee13b6a786c148
M3L6H <sisoac@hotmail.com>
2020-10-12 23:37:18 -0700

    Add append method

--- Lists/Java/LinkedList.java
+++ Lists/Java/LinkedList.java
@@ -1,12 +1,35 @@
 // Singly-linked list implementation in Java
 
-public class LinkedList {
-  private Node root;
+public class LinkedList<T> {
+  private Node<T> root;
   
   public LinkedList() {
     this.root = null;
   }
 
-  class Node {
+  public void append(T value) {
+    if (root == null) {
+      this.root = new Node<T>(value);
+    } else {
+      this.appendRec(value, this.root);
+    }
+  }
+
+  private void appendRec(T value, Node<T> node) {
+    if (node.next == null) {
+      node.next = new Node<T>(value);
+    } else {
+      this.appendRec(value, node.next);
+    }
+  }
+
+  class Node<T> {
+    private T value;
+    public Node<T> next;
+    
+    public Node(T value) {
+      this.value = value;
+      this.next = null;
+    }
   }
 }
