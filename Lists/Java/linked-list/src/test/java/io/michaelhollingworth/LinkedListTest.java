1dfd79877836328f8bfa6f0ebe048ed411324bf3
M3L6H <sisoac@hotmail.com>
2020-10-17 23:37:13 -0700

    Add test file

---
+++ Lists/Java/linked-list/src/test/java/io/michaelhollingworth/LinkedListTest.java
@@ -0,0 +1,12 @@
+package io.michaelhollingworth;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class LinkedListTest {
+  @Test
+  void test() {
+    fail("Not yet implemented!");
+  }
+}
