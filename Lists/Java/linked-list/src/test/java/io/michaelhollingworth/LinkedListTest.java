c47271bcb55bca7695268146f2e75e41abce175e
M3L6H <sisoac@hotmail.com>
2020-10-18 23:55:04 -0700

    Add append test

--- Lists/Java/linked-list/src/test/java/io/michaelhollingworth/LinkedListTest.java
+++ Lists/Java/linked-list/src/test/java/io/michaelhollingworth/LinkedListTest.java
@@ -6,7 +6,20 @@ import org.junit.jupiter.api.Test;
 
 public class LinkedListTest {
   @Test
-  void test() {
-    fail("Not yet implemented!");
+  void testAppend() {
+    LinkedList<Character> list = new LinkedList<Character>();
+
+    // Expect list to start empty
+    assertEquals(0, list.size());
+
+    // Expect list to contain one item after appending
+    list.append('A');
+    assertEquals(1, list.size());
+
+    // Expect list to contain 26 items after appending more items
+    for (char ch: "BCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray()) {
+      list.append(ch);
+    }
+    assertEquals(26, list.size());
   }
 }
