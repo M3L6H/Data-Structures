dfe145bf6c1ca7ba40ee2e3ed4f72009fc871cfd
M3L6H <sisoac@hotmail.com>
2020-10-20 22:04:19 -0700

    Add properties for size and max element

--- Heaps/C#/BinaryHeap.cs
+++ Heaps/C#/BinaryHeap.cs
@@ -1,27 +1,29 @@
 // Implemented as a max-heap (i.e. if comp returns -1 for a and b where a is the
 // parent, they will be swapped)
-public class BinaryHeap<T>
-{
+public class BinaryHeap<T> {
   public delegate int Comp(T a, T b);
+
+  public int Size => _size;
+  public T Max => _size == 0 ? null : _elts[0];
 
   private Comp _comp;
   private T[] _elts;
   private int _size;
   
   public BinaryHeap(Comp comp) {
-    this._comp = comp;
-    this._elts = new T[8];
-    this._size = 0;
+    _comp = comp;
+    _elts = new T[8];
+    _size = 0;
   }
 
   public void Insert(T value) {
-    if (this._size == this._elts.Length) {
-      this.Resize();
+    if (_size == _elts.Length) {
+      Resize();
     }
 
-    this._elts[this._size] = value;
-    this.BubbleUp(this._size);
-    this._size++;
+    _elts[_size] = value;
+    BubbleUp(_size);
+    _size++;
   }
 
   private void BubbleUp(int index) {
@@ -31,18 +33,18 @@ public class BinaryHeap<T>
 
     int parent = (index - 1) / 2;
 
-    if (this._comp(this._elts[parent], this._elts[index]) == -1) {
-      T temp = this._elts[parent];
-      this._elts[parent] = this._elts[index];
-      this._elts[index] = temp;
+    if (_comp(_elts[parent], _elts[index]) == -1) {
+      T temp = _elts[parent];
+      _elts[parent] = _elts[index];
+      _elts[index] = temp;
 
-      this.BubbleUp(parent);
+      BubbleUp(parent);
     }
   }
 
   private void Resize() {
     T[] temp = _elts;
-    _elts = new T[this._elts.length * 2];
+    _elts = new T[_elts.length * 2];
 
     for (int i = 0; i < temp.Length; i++) {
       _elts[i] = temp[i];
