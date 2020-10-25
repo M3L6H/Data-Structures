66633a6f6b621c950ca2c6b06516bad8e5c97af7
M3L6H <sisoac@hotmail.com>
2020-10-24 22:14:57 -0700

    Add basic setup for insert test

--- Heaps/C#/BinaryHeap/BinaryHeapTest.cs
+++ Heaps/C#/BinaryHeap/BinaryHeapTest.cs
@@ -2,12 +2,23 @@ using Xunit;
 
 namespace BinaryHeap
 {
-    public class BinaryHeapTest
+  public class BinaryHeapTest
+  {
+    [Fact]
+    public void TestInsert()
     {
-        [Fact]
-        public void Test1()
-        {
-
+      BinaryHeap<int> heap = new BinaryHeap<int>((int a, int b) => {
+        if (a == b) {
+          return 0;
+        } else if (a > b) {
+          return -1;
+        } else {
+          return 1;
         }
+      });
+
+      Assert.Equal(0, heap.Size);
+      Assert.Equal(0, heap.Max);
     }
+  }
 }
