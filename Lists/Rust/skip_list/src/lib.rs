830f13e527f9d2e1dfcab71f31af3c6d6503497d
M3L6H <sisoac@hotmail.com>
2020-10-28 22:11:47 -0700

    Create outline for structs. Lifetime specifiers are confusing

--- Lists/Rust/skip_list/src/lib.rs
+++ Lists/Rust/skip_list/src/lib.rs
@@ -1,3 +1,18 @@
+//! An implementation of a skip list in Rust.
+//! Supports O(log n) insertion and search while maintaining a list-like structure.
+
+pub struct SkipList<'a, T, F> where F: Fn(&T, &T) -> i32 {
+  root_list: Vec<&'a Node<'a, &'a T>>,
+  size: u32,
+  height: u32,
+  comp: F
+}
+
+struct Node<'a, T> {
+  next_list: Vec<&'a Node<'a, T>>,
+  value: &'a T
+}
+
 #[cfg(test)]
 mod tests {
   #[test]
