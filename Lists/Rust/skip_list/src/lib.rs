118100ed1cae603ea34259c253f83ddb556c75f9
M3L6H <sisoac@hotmail.com>
2020-10-30 23:15:00 -0700

    Refactor skip list into separate module

--- Lists/Rust/skip_list/src/lib.rs
+++ Lists/Rust/skip_list/src/lib.rs
@@ -1,51 +1,11 @@
 //! An implementation of a skip list in Rust.
 //! Supports O(log n) insertion and search while maintaining a list-like structure.
 
-pub struct SkipList<T, F> where F: Fn(&T, &T) -> i32 {
-  root_list: Vec<Node<T>>,
-  size: u32,
-  height: u32,
-  comp: F
-}
-
-impl<T, F> SkipList<T, F> where F: Fn(&T, &T) -> i32 {
-  fn new(comp: F) -> Self {
-    SkipList {
-      root_list: Vec::new(),
-      size: 0,
-      height: 0,
-      comp
-    }
-  }
-
-  fn insert(self: &mut Self, value: T) {
-    if self.size == 0 {
-      self.root_list.push(Node::new(value));
-
-      self.size += 1;
-      self.height = 1;
-      return;
-    }
-  }
-}
-
-struct Node<T> {
-  next_list: Vec<Node<T>>,
-  value: T
-}
-
-impl<T> Node<T> {
-  fn new(value: T) -> Self {
-    Node {
-      next_list: Vec::new(),
-      value
-    }
-  }
-}
+pub mod skip_list;
 
 #[cfg(test)]
 mod tests {
-  use super::SkipList;
+  use super::skip_list::SkipList;
   
   #[test]
   fn it_constructs() {
@@ -61,7 +21,7 @@ mod tests {
 
     let list = SkipList::new(comp);
     
-    assert_eq!(0, list.size);
-    assert_eq!(0, list.height);
+    assert_eq!(0, list.size());
+    assert_eq!(0, list.height());
   }
 }
---
+++ Lists/Rust/skip_list/src/skip_list.rs
@@ -0,0 +1,49 @@
+pub struct SkipList<T, F> where F: Fn(&T, &T) -> i32 {
+  root_list: Vec<Node<T>>,
+  size: u32,
+  height: u32,
+  comp: F
+}
+
+impl<T, F> SkipList<T, F> where F: Fn(&T, &T) -> i32 {
+  pub fn new(comp: F) -> Self {
+    SkipList {
+      root_list: Vec::new(),
+      size: 0,
+      height: 0,
+      comp
+    }
+  }
+
+  pub fn insert(self: &mut Self, value: T) {
+    if self.size == 0 {
+      self.root_list.push(Node::new(value));
+
+      self.size += 1;
+      self.height = 1;
+      return;
+    }
+  }
+
+  pub fn size(self: &Self) -> u32 {
+    self.size
+  }
+
+  pub fn height(self: &Self) -> u32 {
+    self.height
+  }
+}
+
+struct Node<T> {
+  next_list: Vec<Node<T>>,
+  value: T
+}
+
+impl<T> Node<T> {
+  fn new(value: T) -> Self {
+    Node {
+      next_list: Vec::new(),
+      value
+    }
+  }
+}
